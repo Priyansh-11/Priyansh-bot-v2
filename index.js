0168d1d651f75221e389dd97786734cfc2f6d584
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2024-11-24 18:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 
 // sendFile will go here
 app.get('/', function(req, res) {
