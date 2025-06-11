16cdc7bb3e60c8aa4a7de13c82cb78188eb9bd94
Priyansh Rajput <79218606+Priyansh-11@users.noreply.github.com>
2025-06-11 09:15:12 +0530

    Update video.js

--- Priyansh/commands/video.js
+++ Priyansh/commands/video.js
@@ -55,7 +55,7 @@ module.exports = {
 
       // Construct API URL for downloading the top result
       const apiKey = "priyansh-here";
-      const apiUrl = `https://priyansh-ai.onrender.com/youtube?id=${videoId}&type=${type}&apikey=${apiKey}`;
+      const apiUrl = `https://priyanshuapi.xyz/youtube?id=${videoId}&type=${type}&apikey=${apiKey}`;
 
       api.setMessageReaction("⌛", event.messageID, () => {}, true);
 
